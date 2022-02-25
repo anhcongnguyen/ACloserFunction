c933377c6f8badf89c2283fcc266c34640c97fb7
Nguyễn Công Anh <31842967+anhcongnguyen@users.noreply.github.com>
2022-02-25 17:40:47 +0700

    coding challenge

--- script.js
+++ script.js
@@ -75,14 +75,14 @@ const lufthansa = {
     iataCode: 'LH',
     bookings: [],
     book(flightNum, name) {
-        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
+        // console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`);
         this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name })
     }
 }
 
 lufthansa.book(239, 'Jonas Schedtman');
 lufthansa.book(635, 'John Smith');
-console.log(lufthansa);
+// console.log(lufthansa);
 
 const eurowings = {
     airline: 'Eurowings',
@@ -100,10 +100,10 @@ const book = lufthansa.book;
 
 //Call method
 book.call(eurowings, 23, 'Sarah Williams');
-console.log(eurowings);
+// console.log(eurowings);
 
 book.call(lufthansa, 239, 'Marry Cooper');
-console.log(lufthansa);
+// console.log(lufthansa);
 
 const swiss = {
     airline: 'Swiss Air Lines',
@@ -112,13 +112,13 @@ const swiss = {
 }
 
 book.call(swiss, 583, 'Mary Cooper');
-console.log(swiss);
+// console.log(swiss);
 
 // Apply method
 const flightData = [583, 'George Cooper'];
 
 book.apply(swiss, flightData)
-console.log(swiss);
+// console.log(swiss);
 
 book.call(swiss, ...flightData);
 
@@ -139,7 +139,7 @@ lufthansa.planes = 300;
 lufthansa.buyPlane = function () {
     console.log(this);
     this.planes++;
-    console.log(this.planes);
+    // console.log(this.planes);
 };
 
 // lufthansa.buyPlane();
@@ -149,16 +149,83 @@ document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind
 // Partial applicatioin
 
 const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
+// console.log(addTax(0.1, 200));
 
 const addVAT = addTax.bind(null, 0.23);
 
-console.log(addVAT(100));
+// console.log(addVAT(100));
 
 const addTaxRate = (rate) => (value) => value + value * rate;
 
 const addVAT2 = addTaxRate(0.3);
-console.log(addVAT2(200));
+// console.log(addVAT2(200));
+
+
+// CODING CHALLENGE
 
 
 
+
+/* 
+Let's build a simple poll app!
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+Here are your tasks:
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. 
+  For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. 
+  Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. 
+    The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. 
+    If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. 
+    If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+HINT: Use many of the tools you learned about in this and the last section 😉
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+GOOD LUCK 😀
+*/
+
+const poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: Javascript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section 
+    answers: new Array(4).fill(0),
+    registerNewAnswer() {
+        //Answer
+        const answer = Number(
+            prompt(
+                `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+            )
+        );
+        console.log(answer);
+
+        typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++;
+    },
+
+    displayResults(type = 'array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        }
+        if (type === 'string') {
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+        }
+    }
+
+}
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));

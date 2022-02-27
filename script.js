3503635caafd31e571e434d2d31997565aa30a44
Nguyễn Công Anh <31842967+anhcongnguyen@users.noreply.github.com>
2022-02-27 20:22:04 +0700

    final function

--- script.js
+++ script.js
@@ -229,3 +229,104 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 
 
 document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// Immediately invoked function expressions (IIFE)
+const runOne = function () {
+    console.log('This will never run again');
+};
+
+runOne();
+
+//IIFE
+(function () {
+    console.log('This will never run again');
+})();
+
+(() => console.log('This will also never run again'))();
+
+{
+    const isPrivate = 23;
+    var notPrivate = 46;
+}
+// conso.log(isPrivate);
+console.log(notPrivate);
+
+// Closure
+
+const secureBooking = function () {
+    let passengerCount = 0;
+    return function () {
+        passengerCount++;
+        console.log(`${passengerCount} passengers`);
+    }
+}
+
+const booker = secureBooking();
+
+console.dir(booker);
+
+//NOTE
+// A clousure is the closed-over variable environment of the execution context in which a function was created, event after that execution context is gone.
+// A clousure gives a function access to all the variables of its parent function, even after that parent function has returned. The function keeps a reference to its outer scope, which preserves the scope chain throughout time.
+// A clousure makes sure that a function does not loose connection to variables that existed at the function's birth place
+
+
+//Example 1
+let f;
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    }
+}
+
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    }
+}
+
+g();
+f();
+console.dir(f);
+// Re-assigning f function
+h();
+f();
+console.dir(f);
+
+//Example 2
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait}`)
+}
+
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+*/
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', function () {
+        header.style.color = 'blue';
+    });
+})();
+
+
+
+
